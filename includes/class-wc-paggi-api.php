0ef9a80765e6beda11e05dddbf057b8ccb24aa4f
Daniel Galdino <daniel.galdino@altec.ws>
2020-06-15 10:36:16 -0300

    add wc paggi api

--- includes/class-wc-paggi-api.php
+++ includes/class-wc-paggi-api.php
@@ -19,7 +19,7 @@ class WC_Paggi_API {
     /**
      * API URL.
      */
-    const API_URL = 'https://online.paggi.com/api/v4/';
+    // const API_URL = 'https://api.stg.paggi.com/v1';
 
     /**
      * Gateway class.
@@ -29,6 +29,8 @@ class WC_Paggi_API {
     protected $gateway;
 
     protected $enviroment;
+
+    protected $api_url;
 
     /**
      * Constructor.
@@ -60,7 +62,11 @@ class WC_Paggi_API {
      * @return string
      */
     public function get_api_url() {
-        return $this->API_URL;
+        if($this->gateway->sandbox === 'yes') {
+            return 'https://api.stg.paggi.com/v1';
+        } else {
+            return 'https://api.paggi.com/v1';
+        }
     }
 
     /**
@@ -74,297 +80,142 @@ class WC_Paggi_API {
         return preg_replace('([^0-9])', '', $string);
     }
 
-    /**
-     * Perform the comunication with Paggi API
+
+     /**
+     * create a card to a order
      *
      * @since 0.1.0
-     * @param string $action
-     * @param string $method
-     * @param array $data
-     * @return array
-     */
-    public function ws_comunicate($action = null, $method = null, $data = null) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, self::API_URL . '/' . $action);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->gateway->get_token());
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if ($data)
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-        if(array_key_exists('X_FORWARDED_FOR', $_SERVER)) {
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json', "X-Forwarded-For: {$_SERVER['X_FORWARDED_FOR']}"
-          ));
-        }
-        else {
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json', "X-Forwarded-For: {$_SERVER['REMOTE_ADDR']}"
-          ));
-        }
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        $error = curl_error($ch);
-        if ($error != "") {
-            $this->gateway->log->add($this->gateway->id, 'Call Paggi API error: ' . $error);
-            curl_close($ch);
-            return array('code' => '500', 'message' => $error); // erro
-        } else {
-            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            switch ($httpcode) {
-                case '200':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '200';
-                    break;
-                case '204':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '200';
-                    break;
-                case '400':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '400';
-                    $result['Status']['Name'] = 'Bad Request';
-                    $result['Status']['Description'] = __('Something went wrong. Often a required param is missing', 'woocommerce-paggi');
-                    break;
-                case '401':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '401';
-                    $result['Status']['Name'] = 'Unauthorized';
-                    $result['Status']['Description'] = __('Not a valid API Key', 'woocommerce-paggi');
-                    break;
-                case '402':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '402';
-                    $result['Status']['Name'] = 'Request failed';
-                    $result['Status']['Description'] = __('Params were right, but something failed, also used to rejected charges', 'woocommerce-paggi');
-                    break;
-                case '404':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '404';
-                    break;
-                case '410':
-                    curl_close($ch);
-                    $result['message'] = $result;
-                    $result['Status']['Code'] = '410';
-                    $result['Status']['Name'] = 'Not found';
-                    $result['Status']['Description'] = __('Requested item doesn\'t exists', 'woocommerce-paggi');
-                    break;
-                case '422':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '422';
-                    $result['Status']['Name'] = 'Unprocessable Entity';
-                    $result['Status']['Description'] = __('Some param is invalid', 'woocommerce-paggi');
-                    break;
-                case '500':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '500';
-                    $result['Status']['Name'] = 'Server Error';
-                    $result['Status']['Description'] = __('Something went wrong on our servers', 'woocommerce-paggi');
-                    break;
-                case '501':
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = '501';
-                    $result['Status']['Name'] = 'Server Error';
-                    $result['Status']['Description'] = __('Feature not available yet', 'woocommerce-paggi');
-                    break;
-                default:
-                    curl_close($ch);
-                    $result = json_decode($result, true);
-                    $result['Status']['Code'] = 'NA';
-                    $result['Status']['Name'] = __('Not Available', 'woocommerce-paggi');
-                    $result['Status']['Description'] = __('Not Available', 'woocommerce-paggi');
-                    ;
-                    break;
-            }
-        }
-        return $result;
-    }
-
-    /**
-     * Set Customer
-     *
-     * @since 0.1.0
-     * @param string $name
-     * @param string $email
-     * @param string $document
-     * @param string $phone
-     * @param string $street
-     * @param string $district
-     * @param string $city
-     * @param string $state
-     * @param string $zip
-     * @return array
-     */
-    public function set_customer($name, $email, $document, $phone, $street, $district, $city, $state, $zip) {
-        $data = array(
-            'name' => $name,
-            'email' => $email,
-            'document' => $this->only_numbers($document),
-            'phone' => $this->only_numbers($phone),
-            'address' => array(
-                'street' => $street,
-                'neihborhood' => $district,
-                'city' => $city,
-                'state' => $state,
-                'zip' => $zip
-            )
-        );
-
-        $action = 'customers';
-        $method = 'POST';
-        return $this->ws_comunicate($action, $method, $data);
-    }
-
-    /**
-     * Update customer
-     *
-     * @since 0.1.0
-     * @param string $paggi_id
-     * @param string $name
-     * @param string $email
-     * @param string $document
-     * @param string $phone
-     * @param string $street
-     * @param string $district
-     * @param string $city
-     * @param string $state
-     * @param string $zip
-     * @return array
-     */
-    public function update_customer($paggi_id, $name, $email, $document, $phone, $street, $district, $city, $state, $zip) {
-        $data = array(
-            'name' => $name,
-            'email' => $email,
-            'document' => $this->only_numbers($document),
-            'phone' => $this->only_numbers($phone),
-            'address' => array(
-                'street' => $street,
-                'neihborhood' => $district,
-                'city' => $city,
-                'state' => $state,
-                'zip' => $zip
-            )
-        );
-
-        $action = 'customers/' . $paggi_id;
-        $method = 'PUT';
-        return $this->ws_comunicate($action, $method, $data);
-    }
-
-    /**
-     * list Paggi customers
-     *
-     * @since 0.1.0
-     * @param string $paggi_id
-     * @return array
-     */
-    public function list_customer($paggi_id) {
-        $data = null;
-        $action = 'customers/' . $paggi_id;
-        $method = 'GET';
-        return $this->ws_comunicate($action, $method, $data);
-    }
-
-    /**
-     * add card to customer
-     *
-     * @since 0.1.0
-     * @param string $paggi_id
-     * @param string $name
+     * @param string $holder
      * @param string $number
-     * @param string $expiry
+     * @param string $document
+     * @param string $expire
      * @param string $cvc
      * @return array
      */
-    public function set_card($paggi_id, $name, $number, $expiry, $cvc) {
-        $expiry = $this->only_numbers($expiry);
-        $month = substr($expiry, 0, 2);
-        $year = substr($expiry, -2);
+    public function set_card($holder, $document, $number, $expire, $cvc) {
+        $expire = $this->only_numbers($expire);
+        $month = substr($expire, 0, 2);
+        $year = substr($expire, -4);
+        $document = strtr($document, array(
+            '-' => '',
+            '.' => ''
+            )
+        );
         $data = array(
-            'customer_id' => $paggi_id,
-            'name'   => $name,
+            'document' => $document,
+            'holder'   => $holder,
             'number' => $this->only_numbers($number),
             'month'  => $this->only_numbers($month),
             'year'   => $this->only_numbers($year),
             'cvc'    => $this->only_numbers($cvc)
         );
 
-        $action = 'cards';
-        $method = 'POST';
-        return $this->ws_comunicate($action, $method, $data);
+        $resource = new \Paggi\SDK\Card();
+
+        return $resource->create($data);
     }
 
     /**
-     *  exclude a card in Paggi
-     *
-     * @since 0.1.0
-     * @param string $card_id     *
-     */
-    public function del_card($card_id) {
-        if ($card_id == "" && isset($_REQUEST['data'])) {
-            $card_id = $_REQUEST['data'];
-        }
-        $data = null;
-        $action = 'cards/' . $card_id;
-        $method = 'DELETE';
-        $return = $this->ws_comunicate($action, $method, $data);
-        if (isset($return['Status']['Code']) && $return['Status']['Code'] == '200') {
-            wp_send_json(array('code' => '200', 'message' => __('Card deleted successfuly.', 'woocommerce-paggi')));
+    * create a card to a order
+    *
+    * @since 0.1.0
+    * @param string $card_id
+    * @param string $installments
+    * @param string $name
+    * @param string $email
+    * @param string $document
+    * @param string $phone
+    * @param string $street
+    * @param string $neighborhood
+    * @param string $zipcode
+    * @param string $city
+    * @param string $state
+    * @param string $number
+    * @param string $complement
+    * @param string $ip
+    * @param string $external_identifier
+    */
+    public function process_payment($card_id, $installments, $amount, $name, $email, $document, $phone, $street, $neighborhood,
+     $zipcode, $city, $state, $number, $complement, $ip, $external_identifier)
+    {
+        if ($complement !== '' && $neighborhood !== ''){ 
+            $data = array( 
+                'ip' => '127.0.0.1',
+                'external_identifier' => $external_identifier,
+                'charges' => array(
+                    'installments' => $this->only_numbers($installments),
+                    'amount' => $this->only_numbers($amount),
+                    'card' => array (
+                        'id' => $card_id
+                    )
+                ), 
+                'customer' => array(
+                    'name' => $name,
+                    'email' => $email,
+                    'document' => $this->only_numbers($document),
+                    'phone1' => $this->only_numbers($phone),
+                    'address' => array(
+                        'street' => $street,
+                        'neighborhood' => $neighborhood,
+                        'zipcode' => $this->only_numbers($zipcode),
+                        'city' => $city,
+                        'state' => $state,
+                        'number' => $this->only_numbers($number),
+                        'complement' => $complement                    
+                    )
+                )
+            );
         } else {
-            wp_send_json(array('code' => '500', 'message' => __('An error has occurred. Try Again', 'woocommerce-paggi')));
-        }
+            $data = array( 
+                'ip' => '127.0.0.1',
+                'external_identifier' => $external_identifier,
+                'charges' => array(
+                    'installments' => $this->only_numbers($installments),
+                    'amount' => $this->only_numbers($amount),
+                    'card' => array (
+                        'id' => $card_id
+                    )
+                ), 
+                'customer' => array(
+                    'name' => $name,
+                    'email' => $email,
+                    'document' => $this->only_numbers($document),
+                    'phone1' => $this->only_numbers($phone)
+                )
+            );
+            }
+
+        $resource = new \Paggi\SDK\Order();
+
+        return $resource->create($data);    
     }
 
     /**
-     * Send payment request to Paggi
+     * Get order
      *
      * @since 0.1.0
-     * @param string $amount
-     * @param string $customer_id
-     * @param string $card_id
-     * @param string $installments
+     * @param string $partner_id
+     * @param string $order_id
      * @return array
      */
-    public function process_regular_payment($amount, $customer_id, $card_id, $installments) {
-        $data = array(
-            'amount' => $this->only_numbers($amount),
-            'customer_id' => $customer_id,
-            'card_id' => $card_id,
-            'installments_number' => $installments
-        );
 
-        $action = 'charges';
-        $method = 'POST';
-        return $this->ws_comunicate($action, $method, $data);
-    }
 
     /**
-     * Cancel transaction in Paggi
+     * Void order
      *
      * @since 0.1.0
      * @param string $transaction_id
      * @param string $order_id
+     * @return array
      */
+
     public function cancel_regular_payment($transaction_id, $order_id) {
         if ($transaction_id == "" && isset($_REQUEST['data'])) {
             $transaction_id = $_REQUEST['data'];
             $order_id = $_REQUEST['order_id'];
         }
-        $data = null;
-        $action = 'charges/' . $transaction_id . "/cancel";
-        $method = 'PUT';
-        $return = $this->ws_comunicate($action, $method, $data);
+        $resource = new \Paggi\SDK\Order();
+        return $resource->create($data); 
         if (isset($return['status']) && $return['status'] == 'cancelled') {
             $order = new WC_Order($order_id);
             $order->add_order_note(sprintf(__('Paggi: Transaction was canceled (id = %s.)', 'woocommerce-paggi'), $transaction_id));
@@ -375,4 +226,26 @@ class WC_Paggi_API {
         }
     }
 
+
+    /**
+     *  inactivate a card in Paggi
+     *
+     * @since 0.1.0
+     * @param string $card_id     *
+     * @param string $partner_id     *
+     */
+    public function del_card($card_id, $partner_id) {
+        if ($card_id == "" && isset($_REQUEST['data'])) {
+            $card_id = $_REQUEST['data'];
+        }
+        $data = null;
+        $action = 'partners/'.$partner_id.'cards/' . $card_id;
+        $method = 'DELETE';
+        $return = $this->ws_comunicate($action, $method, $data);
+        if (isset($return['Status']['Code']) && $return['Status']['Code'] == '200') {
+            wp_send_json(array('code' => '200', 'message' => __('Card deleted successfuly.', 'woocommerce-paggi')));
+        } else {
+            wp_send_json(array('code' => '500', 'message' => __('An error has occurred. Try Again', 'woocommerce-paggi')));
+        }
+    }
 }
